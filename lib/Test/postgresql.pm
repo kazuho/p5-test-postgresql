692f777ef525d620fb7058ed4b07f264f53f227d
Kazuho Oku <kazuho@li1.kazuhooku.com>
2009-10-15 14:13:17 +0900

    chown tmpdir when running as root

--- lib/Test/postgresql.pm
+++ lib/Test/postgresql.pm
@@ -191,7 +191,12 @@ sub setup {
     my $self = shift;
     # (re)create directory structure
     mkdir $self->base_dir;
-    mkdir $self->base_dir . '/tmp';
+    if (mkdir $self->base_dir . '/tmp') {
+        if ($self->uid) {
+            chown $self->uid, -1, $self->base_dir . '/tmp'
+                or die "failed to chown dir:" . $self->base_dir . "/tmp:$!";
+        }
+    }
     # initdb
     if (! -d $self->base_dir . '/data') {
         pipe my $rfh, my $wfh
