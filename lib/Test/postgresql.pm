72c5816b349e62e5b5bc35428d157a3dd36b3197
root <root@altar.int.qindel.com>
2012-05-09 10:19:13 +0200

    open log file in append mode, otherwise output from the different PostgreSQL processes may overwrite itself

--- lib/Test/postgresql.pm
+++ lib/Test/postgresql.pm
@@ -129,16 +129,16 @@ sub start {
 sub _try_start {
     my ($self, $port) = @_;
     # open log and fork
-    open my $logfh, '>', $self->base_dir . '/postgres.log'
+    open my $logfh, '>>', $self->base_dir . '/postgres.log'
         or die 'failed to create log file:' . $self->base_dir
             . "/postgres.log:$!";
     my $pid = fork;
     die "fork(2) failed:$!"
         unless defined $pid;
     if ($pid == 0) {
-        open STDOUT, '>&', $logfh
+        open STDOUT, '>>&', $logfh
             or die "dup(2) failed:$!";
-        open STDERR, '>&', $logfh
+        open STDERR, '>>&', $logfh
             or die "dup(2) failed:$!";
         chdir $self->base_dir
             or die "failed to chdir to:" . $self->base_dir . ":$!";
